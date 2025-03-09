3a6ab818dacfbc29e2edf80da030f737be87f938
hasitha <hasithasharma24@gmail.com>
2025-03-09 14:52:50 +0530

    edits

--- static.py
+++ static.py
@@ -3,16 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import timedelta
-
-from numba.core.ir import Print
-
 import cost_cosnsolidation
-import numpy as np
 import calendar
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-import plotly.express as px
-from PIL import Image
+
 from pyecharts import options as opts
 from pyecharts.charts import Calendar, Page
 from pyecharts.globals import ThemeType
